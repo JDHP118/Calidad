3ba0cedf12ae5725f407fa1b8fc2e1251039a6e4
juvenalTec <131416988+juvenalTec@users.noreply.github.com>
2023-04-20 20:01:10 -0600

    Update Utilities.java
    Co-authored-by: JDHP118 <88991165+JDHP118@users.noreply.github.com>

--- Utilities.java
+++ Utilities.java
@@ -3,7 +3,7 @@ package com.utils;
 public class Utilities {
   private static final int NUM_FILES = 114;
 
-  private static void findJavaFiles(File parentDirectory, List<File> files) {
+private static void findJavaFiles(File parentDirectory, List<File> files) {
   for(File file : parentDirectory.listFiles()){
     if (file.getName().endsWith(".java"))
       files.add(file);   
